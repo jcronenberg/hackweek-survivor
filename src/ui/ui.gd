c8d9d89d586e1a01b5bb8f06b8656a79ba0400f2
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2025-12-03 00:53:20 +0100

    Add timer to UI

--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -1,5 +1,16 @@
 class_name Ui
 extends Control
 
+var time_elapsed: float = 0.0
+var minutes: int
+var seconds: int
+
+func _physics_process(delta: float) -> void:
+	time_elapsed += delta
+	minutes = int(time_elapsed / 60)
+	seconds = int(time_elapsed) % 60
+	%TimerLabel.text = "%02d:%02d" % [minutes, seconds]
+
+
 func set_game_over(state: bool) -> void:
 	%GameOverLabel.visible = state

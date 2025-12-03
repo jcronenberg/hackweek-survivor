81d9879a14e31e023abcc7e138420be7f9b3557b
Jorik Cronenberg <jorik.cronenberg@suse.com>
2025-12-03 17:32:08 +0100

    Add multishot to ram weapon

--- src/weapons/weapon_ram.gd
+++ src/weapons/weapon_ram.gd
@@ -2,13 +2,29 @@ class_name WeaponRam
 extends Weapon
 
 const RAM_STICK = preload("uid://4ldc5brsy4ed")
+var fire_amount: int = 3
+var spread: float = deg_to_rad(15)
 
 func fire() -> void:
 	var target: Enemy = find_nearest_enemy()
 	if not target:
 		return
 
+	var direction: Vector2 = global_position.direction_to(target.global_position).normalized()
+
+	if fire_amount == 1:
+		_shoot_ram_at(direction)
+		return
+
+	var cur_angle: float = -spread * ((fire_amount - 1) / 2.0)
+
+	for _i in fire_amount:
+		_shoot_ram_at(direction.rotated(cur_angle))
+		cur_angle += spread
+
+
+func _shoot_ram_at(direction: Vector2) -> void:
 	var projectile: RamStick = RAM_STICK.instantiate()
-	projectile.direction = global_position.direction_to(target.global_position).normalized()
+	projectile.direction = direction
 	projectile.global_position = global_position
 	add_child(projectile)

5e4a477ed423c439549fc1afec923f4bb3bc3093
Jorik Cronenberg <jorik.cronenberg@suse.com>
2025-12-04 14:53:45 +0100

    Add audio to xp pickup

--- src/neutral/xp_pickup.gd
+++ src/neutral/xp_pickup.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 func pick_up(player_pos: Vector2) -> void:
 	set_process(false)
 	set_physics_process(false)
+	%AudioStreamPlayer.play()
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", (player_pos + global_position) / 2, 0.05)
 	tween.tween_property(self, "modulate:v", 1, 0.05).from(10)

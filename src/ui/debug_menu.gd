99379b4279dc33cecc19e400debe3cbf4d782299
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2025-12-05 13:38:04 +0100

    Add debug button to increase enemy spawn amount

--- src/ui/debug_menu.gd
+++ src/ui/debug_menu.gd
@@ -4,3 +4,7 @@ extends VBoxContainer
 func _on_level_up_button_pressed() -> void:
 	var player: Player = Global.get_player()
 	player.xp += player.next_level - player.xp
+
+
+func _on_spawn_amount_button_pressed() -> void:
+	get_node("/root/Game").spawn_amount += 1
